cbdf621583994f66a7ac2d49573ef0deac414b9a
akahles <akahles@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2011-06-22 07:59:44 +0000

    * fixed typos * added possibility for direct creation of the filtered file

--- optimal_filter_set.sh
+++ optimal_filter_set.sh
@@ -4,7 +4,11 @@ set -e
 
 if [ -z "$1" ]
 then
-    echo "usage: $0 <annotation in GFF3> <alignment in SAM>"
+    echo "usage: $0 <annotation in GFF3> <alignment in SAM> (<filter outfile>)')"
+    echo ""
+    echo "       if <filter outfile> is given, the optimal filter criteria will"
+    echo "       be applied to the given alignment set and the result ist stored"
+    echo "       in <filter outfile>."
     exit 1
 else
     annotation="$1"
@@ -12,12 +16,17 @@ fi
 
 if [ -z "$2" ]
 then
-    echo "usage: $0 <annotation in GFF3> <alignment in SAM>"
+    echo "usage: $0 <annotation in GFF3> <alignment in SAM> (<filter outfile>)')"
+    echo ""
+    echo "       if <filter outfile> is given, the optimal filter criteria will"
+    echo "       be applied to the given alignment set and the result ist stored"
+    echo "       in <filter outfile>."
     exit 1
 else
     alignment="$2"
 fi
 
+filter_out="$3"
 
 echo "Generating annotation intron list"
 echo ""
@@ -31,7 +40,7 @@ echo "done"
 echo ""
 
 
-echo "Gernerating alignment feature list"
+echo "Generating alignment feature list"
 echo ""
 
 if [ ! -f ${alignment}.features ]
@@ -47,3 +56,23 @@ echo ""
 python find_optimal_param_set.py -v -i ${annotation}.introns -f ${alignment}.features -b ${alignment}.best_score -m ${alignment}.scoring_matrix
 echo "done"
 echo ""
+
+if [ ! -z "$filter_out" ]
+then
+    echo "Filter Alignment"
+    echo ""
+    min_ex_len=`tail -n 1 ${alignment}.best_score | cut -f 1`
+    max_mm=`tail -n 1 ${alignment}.best_score | cut -f 2`
+    min_support=`tail -n 1 ${alignment}.best_score | cut -f 3`
+    support_string=""
+
+    if [ "$min_support" != "1" ]
+    then
+        python filter_features.py -e $min_ex_len -X $max_mm -m $min_support -i ${alignment}.features -o ${alignment}.features_filtered
+        support_string="-i ${alignment}.features_filtered"
+    fi
+
+    python filter_alignement.py -a $alignment -o $filter_out -e $min_ex_len -X $max_mm $support_string
+    echo ""
+fi
+
