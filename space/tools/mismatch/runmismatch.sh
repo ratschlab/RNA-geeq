8e7db9eafdb4523eca84cea2568651dfbab4e7bd
Gunnar Raetsch <raetsch@cbio.mskcc.org>
2012-12-31 16:14:32 -0800

    changed paths from /fml/ag-raetsch to /cbio/grlab

--- space/tools/mismatch/runmismatch.sh
+++ space/tools/mismatch/runmismatch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /fml/ag-raetsch/nobackup/projects/rgasp/reads
+cd /cbio/grlab/nobackup/projects/rgasp/reads
 for i in `find ./ -name read_sample.result` ; do python ~/svn/projects/rgasp/read_processing/mismatch/mismatch.py $i `dirname $i`/mismatch.pickle ; done
 for i in `find ./ -name mismatch.pickle` ; do python ~/svn/projects/rgasp/read_processing/mismatch/plot_mismatch.py $i `dirname $i`/mismatch.pdf ; done
